4d5ac894400b5dcefb0e2f29eb47bdf936c01fe7
Florian Hartig <florianhartig@users.noreply.github.com>
2014-02-28 14:53:25 +0700

    Committed project, settings and data cleaning scripts

---
+++ code/01-settings.r
@@ -0,0 +1,16 @@
+#' script with basic settings. Change rootdirectory to your project location
+#'
+#' @author Florian Hartig \url{http://florianhartig.wordpress.com/}
+
+
+library(plyr)
+library(boot)
+library(lme4)
+library(vegan)
+library(foreach)
+
+rootdirectory <- "/Users/Florian/Home/Projekte/Papers_in_Progress/14-WirongCrownShape"
+setwd(rootdirectory)
+
+
+
